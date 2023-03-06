deee43a59306696e2d17fea0a478074145840632
veronika-simic <veronikasimic21@gmail.com>
2023-03-06 11:43:14 +0100

    initialize main components

--- space-tourism/src/app/app.module.ts
+++ space-tourism/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { DestinationsComponent } from './destinations/destinations.component';
+import { CrewComponent } from './crew/crew.component';
+import { TechnologyComponent } from './technology/technology.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    DestinationsComponent,
+    CrewComponent,
+    TechnologyComponent
   ],
   imports: [
     BrowserModule,
