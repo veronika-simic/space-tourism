6965a367c2bd542d8e6f5ca93050da9b508885eb
veronika-simic <veronikasimic21@gmail.com>
2023-03-07 09:46:39 +0100

    add navigation links

--- space-tourism/src/app/app-routing.module.ts
+++ space-tourism/src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CrewComponent } from './crew/crew.component';
+import { DestinationsComponent } from './destinations/destinations.component';
 import { HomePageComponent } from './home-page/home-page.component';
+import { TechnologyComponent } from './technology/technology.component';
 
 const routes: Routes = [
-  {path: '', redirectTo: '/', pathMatch: 'full'},
-  {path: 'crew', component: CrewComponent}
+  {path: '', component: HomePageComponent},
+  {path: 'home', component: HomePageComponent},
+  {path: 'destination', component: DestinationsComponent},
+  {path: 'technology', component: TechnologyComponent},
+  {path: 'crew', component: CrewComponent},
 ];
 
 @NgModule({
